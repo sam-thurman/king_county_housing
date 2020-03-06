9052e4501278f665edc55072484953d6a983f9f8
Sam Thurman <sam.bbmgmt@gmail.com>
2020-03-05 16:19:58 -0800

    the merge-ening

--- src/stat_helpers.py
+++ src/stat_helpers.py
@@ -12,8 +12,6 @@ from statsmodels.formula.api import ols
 from statsmodels.regression.linear_model import OLS
 from statsmodels.stats.diagnostic import linear_rainbow, het_breuschpagan
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-
-from sklearn.linear_model import LinearRegression
 #-------------------------#
 
 
@@ -48,20 +46,31 @@ def bootstrapped_ttest_bin(df, col_name):
     return 'p-value: ', np.array(Ps).mean(), 'statistic: ', np.array(Ts).mean()
 
 
-# def get_VIF(df, cols):
+def get_VIF(df, cols):
 
-#     columns = cols
-#     rows = df[columns].values
-#     vif_df = pd.DataFrame()
-#     vif_df['Feature'] = columns
-#     vif_df['VIF'] = [variance_inflation_factor(
-#         rows, i) for i in range(len(columns))]
+    columns = cols
+    rows = df[columns].values
+    vif_df = pd.DataFrame()
+    vif_df['Feature'] = columns
+    vif_df['VIF'] = [variance_inflation_factor(
+        rows, i) for i in range(len(columns))]
 
-#     return vif_df
+    return vif_df
 
 
-# def generate_ols_summary(df, col_list):
-#     string = 'SalePrice ~ '
-#     string += ' + '.join(col_list)
-#     fsm = ols(formula=string, data=df).fit()
-#     return fsm
+def generate_ols_summary(df, col_list):
+    string = 'SalePrice ~ '
+    string += ' + '.join(col_list)
+    fsm = ols(formula=string, data=df).fit()
+    return fsm
+
+
+def cohen_d(x, y):
+    nx = len(x)
+    ny = len(y)
+    sx = np.std(x)
+    sy = np.std(y)
+    numer = (nx-1)*sx**2 + (ny-1)*sy**2
+    denom = nx + ny - 2
+    pool_std = np.sqrt(numer/denom)
+    return abs(np.mean(x) - np.mean(y))/pool_std
--- src/wrangle_helpers.py
+++ src/wrangle_helpers.py
@@ -11,64 +11,82 @@ import statsmodels.stats as stats
 from statsmodels.formula.api import ols
 from statsmodels.stats.diagnostic import linear_rainbow, het_breuschpagan
 from statsmodels.stats.outliers_influence import variance_inflation_factor
-
-from sklearn.linear_model import LinearRegression
 #-------------------------#
 
+
 def num_to_bin(list_of_cols):
-  '''
-  This function takes dataframe columns with numeric values representing categorical data.
-  It returns the dataframe with the column values converted to binary: 1s representing the presence 
-  of a feature and 0s representing that the house did not have this feature
-  '''
-  for _ in range(len(list_of_cols)):
-    list_of_cols[_] = list_of_cols[_].astype(bool).astype(int)
-  return list_of_cols
+    '''
+    This function takes dataframe columns with numeric values representing categorical data.
+    It returns the dataframe with the column values converted to binary: 1s representing the presence
+    of a feature and 0s representing that the house did not have this feature
+    '''
+    for _ in range(len(list_of_cols)):
+        list_of_cols[_] = list_of_cols[_].astype(bool).astype(int)
+    return list_of_cols
+
 
 def filter_and_groom(unfiltered_df):
-  '''
-  selecting interested columns
-  '''
-  groomed_df = unfiltered_df[['pin', 'SalePrice', 'SqFtTotLiving', 'SqFtOpenPorch', 
-                          'SqFtEnclosedPorch', 'SqFtDeck', 'Bedrooms', 'BathHalfCount', 
-                          'Bath3qtrCount', 'BathFullCount','TidelandShoreland',
-                          'TrafficNoise', 'Stories', 'Condition', 'Area', 
-                          'DocumentDate', 'MtRainier', 'Olympics', 'Cascades', 
-                          'Territorial', 'SeattleSkyline', 'PugetSound', 
-                          'LakeWashington', 'LakeSammamish', 'SmallLakeRiverCreek', 
-                          'OtherView','WaterSystem', 'SewerSystem', 'OtherProblems', 'YrBuilt', 
-                          'YrRenovated', 'BldgGrade', 'Township', 'FpSingleStory', 'FpMultiStory']]
-  '''
-  convert 'otherproblems' column (Ys, Ns) into 1s and 0s
-  '''
-  mylist = groomed_df['OtherProblems']
-  for (i, item) in enumerate(mylist):
-    if item == 'N':
-        mylist[i] = 0
-    elif item == 'Y':
-        mylist[i] = 1
-  groomed_df['OtherProblems'] = mylist
+    '''
+    selecting interested columns
+    '''
+    groomed_df = unfiltered_df[['pin', 'SalePrice', 'SqFtTotLiving', 'SqFtOpenPorch',
+                                'SqFtEnclosedPorch', 'SqFtDeck', 'Bedrooms', 'BathHalfCount',
+                                'Bath3qtrCount', 'BathFullCount', 'PowerLines', 'TrafficNoise',
+                                'AirportNoise', 'TidelandShoreland', 'Stories',
+                                'Condition', 'Area', 'DocumentDate', 'MtRainier', 'Olympics', 'Cascades',
+                                'Territorial', 'SeattleSkyline', 'PugetSound',
+                                'LakeWashington', 'LakeSammamish', 'SmallLakeRiverCreek',
+                                'OtherView', 'WaterSystem', 'SewerSystem', 'OtherProblems', 'YrBuilt',
+                                'YrRenovated', 'BldgGrade', 'Township', 'FpSingleStory', 'FpMultiStory']]
+    '''
+    convert OtherProblems and PowerLines column (Ys, Ns) into 1s and 0s
+    '''
 
-  township_mean_sale_prices = {}
-  for town in unfiltered_df['Township'].value_counts().index:
-    township_mean_sale_prices[town] = groomed_df[groomed_df['Township'] == town]['SalePrice'].mean()
-  groomed_df['TownshipMeanSalePrice'] = groomed_df['Township'].map(township_mean_sale_prices)
-  groomed_df['TownshipMeanSalePrice'] = groomed_df['TownshipMeanSalePrice'].astype(int)
-  groomed_df['ExpensiveForArea?'] = (groomed_df['SalePrice'] >= groomed_df['TownshipMeanSalePrice']).astype(int)
+    for (i, item) in enumerate(groomed_df['OtherProblems']):
+        if item == 'N':
+            groomed_df['OtherProblems'][i] = 0
+        elif item == 'Y':
+            groomed_df['OtherProblems'][i] = 1
+    for (i, item) in enumerate(groomed_df['PowerLines']):
+        if item == 'N':
+            groomed_df['PowerLines'][i] = 0
+        elif item == 'Y':
+            groomed_df['PowerLines'][i] = 1
 
+    # ViewScore is a composite of three views (SeattleSkyline, LakeWashington, LakeSammamish).  These three views were found to have the most impact on sale price and
+    # capturing variance in our model so we decided to use these three values summed as it's own feature.  This is not a
+    # score for the total quality of view from the house, simply views of these three features.
+    columns = ['SeattleSkyline', 'LakeWashington', 'LakeSammamish']
+    groomed_df['ViewScore'] = 0
+    for column in columns:
+        groomed_df['ViewScore'] = groomed_df['ViewScore'] + groomed_df[column]
 
-  groomed_df = groomed_df[groomed_df['SqFtTotLiving'] < 50000]
-  groomed_df['PricePerSqFt'] = groomed_df['SalePrice']/groomed_df['SqFtTotLiving']
-  groomed_df['TidelandShoreland'].value_counts()
-  groomed_df['TidelandShoreland'], groomed_df['TrafficNoise'] = num_to_bin([groomed_df['TidelandShoreland'], groomed_df['TrafficNoise']])
-  groomed_df['YrRenovated'] = groomed_df['YrRenovated'].astype(int)
+    township_mean_sale_prices = {}
+    for town in unfiltered_df['Township'].value_counts().index:
+        township_mean_sale_prices[town] = groomed_df[groomed_df['Township']
+                                                     == town]['SalePrice'].mean()
+    groomed_df['TownshipMeanSalePrice'] = groomed_df['Township'].map(
+        township_mean_sale_prices)
+    groomed_df['TownshipMeanSalePrice'] = groomed_df['TownshipMeanSalePrice'].astype(
+        int)
+    groomed_df['ExpensiveForArea?'] = (
+        groomed_df['SalePrice'] >= groomed_df['TownshipMeanSalePrice']).astype(int)
 
-  return groomed_df
+    groomed_df = groomed_df[groomed_df['SqFtTotLiving'] < 50000]
+    groomed_df['PricePerSqFt'] = groomed_df['SalePrice'] / \
+        groomed_df['SqFtTotLiving']
+    groomed_df['TidelandShoreland'].value_counts()
+    groomed_df['TidelandShoreland'], groomed_df['TrafficNoise'] = num_to_bin(
+        [groomed_df['TidelandShoreland'], groomed_df['TrafficNoise']])
+    groomed_df['YrRenovated'] = groomed_df['YrRenovated'].astype(int)
+
+    return groomed_df
+
 
 def load_and_wrangle_data(file_path):
-  '''
-  
-  '''
-  ungroomed_df = pd.read_csv(file_path, encoding='latin-1')
-  groomed_df = filter_and_groom(ungroomed_df)
-  return ungroomed_df, groomed_df
+    '''
+
+    '''
+    ungroomed_df = pd.read_csv(file_path, encoding='latin-1')
+    groomed_df = filter_and_groom(ungroomed_df)
+    return ungroomed_df, groomed_df
